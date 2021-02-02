1e8d57b5e124d4df27a07b956440536ba1264e1f
tubaaktas <tubaaktas83@gmail.com>
2021-02-02 18:42:06 +0300

    Eklendi.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Oracle,SqlServer,Postgres,MongoDb
+            _products = new List<Product> {
+            new Product{ProductId=1, CategoryId=1,ProductName="Bardak",UnitPrice=15, UnitsInStock=15 },
+            new Product{ProductId=2, CategoryId=1,ProductName="Kamera",UnitPrice=500, UnitsInStock=3 },
+            new Product{ProductId=3, CategoryId=2,ProductName="Telefon",UnitPrice=1500, UnitsInStock=2 },
+            new Product{ProductId=4, CategoryId=2,ProductName="Klavye",UnitPrice=150, UnitsInStock=65 },
+            new Product{ProductId=5, CategoryId=2,ProductName="Fare",UnitPrice=85, UnitsInStock=1 }
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _products.Remove(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void UpDate(Product product)
+        {
+        }
+    }
+}
