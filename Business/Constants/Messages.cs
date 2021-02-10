90b7593339fc26795c4d1c50f8ac13c08c83de8d
tubaaktas <tubaaktas83@gmail.com>
2021-02-11 00:27:56 +0300

    10. hafta güncel

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Add products";
+        public static string ProductNameInvalid = "Invalid product name ";
+        public static string MaintenanceTime = "System maintenance ";
+        public static string ProductsListed = "List products";
+    }
+}
---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Core.DataAccess;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TConext> : IEntityRepository<TEntity>
+        where TEntity:class, IEntity,new()
+        where TConext:DbContext , new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TConext context = new TConext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TConext context = new TConext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TConext context = new TConext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TConext context = new TConext())
+            {
+                return filter == null ?
+                    context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void UpDate(TEntity entity)
+        {
+            using (TConext context = new TConext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
